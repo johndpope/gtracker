6b9786f72b48112935ff881c538c1185e7079401
Dmitry S. Melnikov <dmitryme@gtracker.(none)>
2010-10-14 11:56:21 +0400

    pub added

--- server/db/src/gtracker_db_pub.erl
+++ server/db/src/gtracker_db_pub.erl
@@ -1,1 +1,17 @@
 -module(gtracker_db_pub).
+
+-export(
+   [
+      new_device/1
+      ,select_device/2
+      ,select_all_devices/1
+   ]).
+
+new_device(ServerRef) ->
+   gen_server:call(ServerRef, new_device).
+
+select_device(ServerRef, DevName) ->
+   gen_server:call(ServerRef, {get_device, DevName}).
+
+select_all_devices(ServerRef)->
+   gen_server:call(ServerRef, get_all_devices).
