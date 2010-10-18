508454a40320f019cb37412f82e8fd25a7b8c91f
Sergey Kovalevich <indie@gtracker.(none)>
2010-10-19 00:02:26 +0400

    yupi

--- www2/nitrogen/site/src/elements/element_map.erl
+++ www2/nitrogen/site/src/elements/element_map.erl
@@ -6,13 +6,7 @@
 reflect() -> record_info(fields, map).
    
 render_element(R = #map{}) ->
-   %wf:wire(wf:f("$(~p).css('background', 'red');", [ R#map.id ])),
-   MapId = wf:temp_id(),
-   Body = #panel{ id=R#map.id, body=[
-         wf:f("<div id=~p style=~p class=~p></div>", [MapId, R#map.style, R#map.class])
-      ]},
-
-   wf:wire(wf:f("var _map = new Map.Map(~p); _map.setCenter(~f,~f); _map.setZoom(~w); $~w = _map;",
-         [ MapId, R#map.lon, R#map.lat, R#map.zoom, MapId ])),
-
+   Body = #panel{ id=R#map.id, style=R#map.style, class=R#map.class },
+   wf:wire(wf:f("createMap(obj('me'), ~f, ~f, ~w);",
+         [ R#map.lon, R#map.lat, R#map.zoom ])),
    Body.
--- www2/nitrogen/site/src/map.erl
+++ www2/nitrogen/site/src/map.erl
@@ -2,12 +2,16 @@
 -compile(export_all).
 -include_lib("nitrogen/include/wf.hrl").
 -include("records.hrl").
+-define(MAP_STYLE, "height: 480px; border: 1px solid black;").
 
 main() ->
    #template { file="./site/templates/index.html" }.
+
+script() ->
+   #template { file="./site/templates/map_script.html" }.
 
 title() ->
    "GTracker".
 
 body() ->
-   #map { id=map, style="height: 480px; border: 1px solid black;" }.
+   #map { id=map, style=?MAP_STYLE }.
